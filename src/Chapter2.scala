a732c452f9804e94cf7b85024bcee6fef40c9adc
Clark Rasmussen <crasmussen@constantcontact.com>
2015-03-28 15:44:30 -0700

    Factorial and Fibonacci.

--- src/Chapter2.scala
+++ src/Chapter2.scala
@@ -7,20 +7,21 @@ object Chapter2 {
 
   def fib(n: Int): Int = {
     def go(n: Int, v1: Int, v2: Int): Int =
-      if (n < 1) -1
-      else if (n == 1) 0
-      else if (n == 1) v2
+      if (n == 2) v2
       else go(n - 1, v2, v1 + v2)
-    go(n, 0, 1)
+    if (n < 1) -1
+    else if (n == 1) 0
+    else go(n, 0, 1)
   }
 
   def main(args: Array[String]): Unit = {
-    println(fib(0))
+    println("factorial:", factorial(5))
+    println(fib(0)) // should result in error (-1)
     println(fib(1))
     println(fib(2))
     println(fib(3))
     println(fib(4))
     println(fib(5))
-    println("factorial:", factorial(5))
+    println(fib(6))
   }
 }
